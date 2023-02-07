aa8bb5836b629f43bc52df35a4ddf6a54a4d195b
alvar soler <alvardsoler@gmail.com>
2023-02-07 14:02:41 +0000

    añadido logger

--- arduino_connector.py
+++ arduino_connector.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # to do: - reconnect when connection closes
-import serial,time
-import os, sys, datetime
-import mariadb
+import serial, time, os, sys, datetime, mariadb, logging
 from config import *
 
 ttyPORT = '/dev/ttyUSB0' if os.path.exists('/dev/ttyUSB0') else '/dev/ttyACM0';
@@ -18,13 +16,13 @@ try:
             database=DB_SCHEMA)
     cursor = conn.cursor()
 except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
+    logger.info(f"Error connecting to MariaDB Platform: {e}")
     sys.exit(1)
 
 # Read from ttyPORT
 def read (arduino):
     time.sleep(0.1)
-    # print('Reading...')
+    # logger.info('Reading...')
     while arduino.inWaiting() == 0:
         time.sleep(0.1)
         pass
@@ -33,10 +31,10 @@ def read (arduino):
         if (not msg): pass
 
         dataMsg = str(msg.decode('utf-8').rstrip())
-        # print(dataMsg)
+        # logger.info(dataMsg)
         arduino.flushInput() #remove data after reading
         temp, hum = dataMsg.split(',')
-        # print(f'Temperature: {temp}, Humidity: {hum}')
+        # logger.info(f'Temperature: {temp}, Humidity: {hum}')
         saveToDB(temp, hum)
 
 # store observations in DB
@@ -47,7 +45,7 @@ def saveToDB (temp, hum):
         try:
             cursor.execute("INSERT INTO observations(date, temperature, humidity) VALUES (now(), ?, ?);", (temp, hum))  
         except mariadb.Error as e:
-            print(f"Error: {e}")
+            logger.info(f"Error: {e}")
 
 # returns max and min temperature values from the last 24h
 def getMinMaxTemp ():
@@ -56,20 +54,24 @@ def getMinMaxTemp ():
         for minTemp, maxTemp in cursor:
             return { 'min': minTemp, 'max': maxTemp }
     except mariadb.Error as e:
-        print(f"Error: {f}")
+        logger.info(f"Error: {f}")
 
 # main function
 if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
     if not os.path.exists(ttyPORT):
-        print('No arduino connected! Going out...')
+        logger.info('No arduino connected! Going out...')
         exit(0)
-    print('Running. Press CTRL-C to exit.')
+    logger.info('Running. Press CTRL-C to exit.')
 
     with serial.Serial(ttyPORT, 9600, timeout=1) as arduino:
         arduino.flush()
         time.sleep(0.1) #wait for serial to open
         if arduino.isOpen():
-            print("{} connected!".format(arduino.port))
+            logger.info("{} connected!".format(arduino.port))
             try:
                 while True:
                     read(arduino) # dentro hay un sleep
@@ -77,9 +79,9 @@ if __name__ == '__main__':
                     min = minMax['min']
                     max = minMax['max']
                     msg = f'{min},{max}'
-                    # print(f'Sending to arduino "{msg}"')
+                    # logger.info(f'Sending to arduino "{msg}"')
                     arduino.write(msg.encode())
                                                         
             except KeyboardInterrupt:
-                print("KeyboardInterrupt has been caught.")
+                logger.info("KeyboardInterrupt has been caught.")
                 conn.close()
